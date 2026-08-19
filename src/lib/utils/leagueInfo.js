0d9f7e911a4ec82ac01beb7d983df2580fe2ab6f
nsab22 <nicksabato22@gmail.com>
2026-08-19 03:05:31 -0400

    Remove rival details from leagueInfo.js
    Removed rival information from league info structure.

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -46,10 +46,6 @@ export const managers = [
       "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in [...[...]
-      "rival": {
-        "name": "Rival", // Can be anything (usually your rival's name)
-        "link": 6, // manager array number within this array, or null to link back to all managers page
-        "image": "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
