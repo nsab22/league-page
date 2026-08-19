1730152aa702d315acd5c4346a14acd589107fad
nsab22 <nicksabato22@gmail.com>
2026-08-19 02:37:42 -0400

    Remove optional dues export from leagueInfo.js
    Removed optional dues export from leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "1312138583477346304"; // your league ID
 export const leagueName = "Level Below Pro League"; // your league name
-export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
